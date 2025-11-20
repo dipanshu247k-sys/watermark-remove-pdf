007323dda00c820a4a51e07b6b466a45df3170eb
Dipanshu Kumar <dipanshu247k@gmail.com>
2025-11-20 23:18:56 +0530

    quite final

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 cd ~/wrp
-pkg install -y python libjpeg-turbo termux-api poppler
+pkg install -y python libjpeg-turbo termux-api poppler mupdf
 pip install Pillow
 rm -rf /data/data/com.termux/files/usr/etc/motd
 termux-setup-storage
--- src/start.sh
+++ src/start.sh
@@ -1,14 +1,32 @@
 #!/data/data/com.termux/files/usr/bin/bash
 mkdir -p ~/wrp/trash
 cd ~/wrp/trash
+
 termux-storage-get sample.pdf
+if [ $? -ne 0 ]; then
+  echo "Please Choose a file"
+  exit 1
+fi
+
 pdfimages -j sample.pdf s
+if [ $? -ne 0 ]; then
+  echo "Error: pdfimages command failed."
+  mupdf sample.pdf sample_fixed.pdf
+  pdfimages -j sample_fixed.pdf s
+fi
+
 bash ../src/duplicate_remover.sh .
+
 read -p "Enter the filename: " filename
-$filename="$filename".pdf
+filename="$filename".pdf
+
 python ~/wrp/src/img2pdf.py $(ls -v *.jpg) -o "$filename"
+
 mkdir -p /storage/emulated/0/Documents/processed/
 mv "$filename" /storage/emulated/0/Documents/processed/
+
 termux-open /storage/emulated/0/Documents/processed/"$filename"
+
 rm -rf *
-echo "Check /storage/Documents/processed/$file_name"
+
+echo "Check /storage/Documents/processed/$filename"
