b151f9a6eac45161a02171825b0cfb95d11b4486
Dipanshu Kumar <dipanshu247k@gmail.com>
2025-11-20 20:04:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 pkg install -y python libjpeg-turbo termux-api poppler
 pip install Pillow
 rm -rf /data/data/com.termux/files/usr/etc/motd
-until termux-setup-storage; do
-    echo "Command failed, retrying..."
+termux-setup-storage
 done
 
