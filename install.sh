4630c6301762fd2e4770c31703abb417a4278927
Dipanshu Kumar <dipanshu247k@gmail.com>
2025-11-20 19:12:02 +0530

    Removed Unwanted dependencies

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 #pkg update -y && pkg upgrade -y
-pkg install -y python ndk-sysroot clang make libjpeg-turbo termux-api poppler
+pkg install -y python libjpeg-turbo termux-api poppler
 LDFLAGS="-L/system/lib/" CFLAGS="-I/data/data/com.termux/files/usr/include/" pip install Pillow || pip install Pillow
 clang ~/.watermark-remove-pdf/src/pdfrm.c -o /data/data/com.termux/files/usr/bin/pdfrm
 rm -rf /data/data/com.termux/files/usr/etc/motd
