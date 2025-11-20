9c45d9d1bbc6d43e250227a752ef81b559e61802
Dipanshu Kumar <dipanshu247k@gmail.com>
2025-11-20 19:16:47 +0530

    made installation script better

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
-#pkg update -y && pkg upgrade -y
 pkg install -y python libjpeg-turbo termux-api poppler
-LDFLAGS="-L/system/lib/" CFLAGS="-I/data/data/com.termux/files/usr/include/" pip install Pillow || pip install Pillow
-clang ~/.watermark-remove-pdf/src/pdfrm.c -o /data/data/com.termux/files/usr/bin/pdfrm
+pip install Pillow
 rm -rf /data/data/com.termux/files/usr/etc/motd
 until termux-setup-storage; do
     echo "Command failed, retrying..."
 done
-exit
+
