79f652cd3719b6e6db9f598446c1df55296d52fc
Dipanshu Kumar <dipanshu247k@gmail.com>
2025-11-21 14:34:19 +0530

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -2,24 +2,32 @@
 mkdir -p ~/wrp/trash
 cd ~/wrp/trash
 
+read -p "Enter the filename: " filename
+filename="$filename".pdf
+
 termux-storage-get sample_un.pdf
 while [ ! -f sample_un.pdf ]; do
-  echo "Waiting for sample.pdf..."
   sleep 1
 done
+echo "Detected File"
 
 if [ "$1" = "fix" ]; then
     qpdf sample_un.pdf sample.pdf
+    echo "Fixing the file"
 else
     mv sample_un.pdf sample.pdf
 fi
 pdfimages -j sample.pdf s
+echo "Extracted all images"
 
+if [ "$1" != "fix" ]; then
+    rm *.ppm
+fi
+echo "Checking for duplicates"
 bash ../src/duplicate_remover.sh
 
-read -p "Enter the filename: " filename
-filename="$filename".pdf
 
+echo "Making PDF file from images"
 python ~/wrp/src/img2pdf.py $(ls -v *.jpg) -o "$filename"
 
 mkdir -p /storage/emulated/0/Documents/processed/
