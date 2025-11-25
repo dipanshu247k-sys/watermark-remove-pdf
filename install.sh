fcc58b25da6018ed2b1de1f6c2e52892fac9241b
Dipanshu Kumar <dipanshu247k@gmail.com>
2025-11-25 17:16:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 cd ~/wrp
+pkg update
 pkg install -y python libjpeg-turbo termux-api poppler qpdf
 pip install Pillow
 rm -rf /data/data/com.termux/files/usr/etc/motd
